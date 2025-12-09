e05f81fcea04434510491f2953eb6c6677396125
yusuf <yusuf@DESKTOP-ED3PSDF>
2025-12-09 20:35:29 +0300

    complated token service

--- RylozeWebSecureLoginApp.API/Services/TokenService.cs
+++ RylozeWebSecureLoginApp.API/Services/TokenService.cs
@@ -1,6 +1,11 @@
 ﻿using System.Security.Claims;
 using RylozeWebSecureLoginApp.API.Entities;
 using RylozeWebSecureLoginApp.API.Interfaces;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace RylozeWebSecureLoginApp.API.Services
 {
@@ -15,17 +20,76 @@ namespace RylozeWebSecureLoginApp.API.Services
 
         public string GenerateAccessToken(AppUser user, IList<string> roles)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName!)
+            };
+
+            if (!string.IsNullOrEmpty(user.FullName))
+            {
+                claims.Add(new Claim("FullName", user.FullName));
+            }
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SecretKey"]!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.Now.AddMinutes(
+                int.Parse(_config["JWT:AccessTokenExpirationMinutes"]!));
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: creds,
+                notBefore: DateTime.Now
+            );
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomBytes = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomBytes);
+            return Convert.ToBase64String(randomBytes);
         }
 
         public ClaimsPrincipal? GetPrincipalFromExpiredToken(string token)
         {
-            throw new NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidIssuer = _config["JWT:Issuer"],
+                ValidAudience = _config["JWT:Audience"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SecretKey"]!)),
+                ValidateLifetime = false // Süresi dolmuş tokenleri parse etmek için bunu false veriyoruz.
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtToken ||
+                ! jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return null;
+            }
+
+            return principal;
         }
     }
 }
